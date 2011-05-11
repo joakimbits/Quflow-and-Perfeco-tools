f84c6e65da3b302d5dd3039d182b255c0c971ec7
Joakim Pettersson <joakimbits@gmail.com>
2011-05-11 16:53:16 +0200

    Added expansion of subexpressions and rescale for symbolic units.

--- lib/algebra.py
+++ lib/algebra.py
@@ -1,4 +1,4 @@
-#https://github.com/joakimbits/Quflow-and-Perfeco-tools/blob/master/algebra.py
+#https://github.com/joakimbits/Quflow-and-Perfeco-tools/lib/algebra.py
 """
 Algebra with symbols, physical constants, arrays and uncertainties
 """
@@ -7,6 +7,7 @@ from re import compile
 from sympy import Basic, Symbol, Matrix, sympify, lambdify, solve
 from collections import Iterable
 from quantities import Quantity
+from numpy import ndarray, array
 from quantities.unitquantity import \
      UnitCurrency, UnitCurrent, UnitInformation, UnitLength, \
      UnitLuminousIntensity, UnitMass, UnitSubstance, UnitTemperature, \
@@ -18,191 +19,166 @@ dimensions = (UnitCurrent, UnitInformation, UnitLength,
               UnitTemperature, UnitTime)
 _Mul = compile(r'([A-Za-z0-9_)])(\s+)(?=[A-Za-z0-9_(])').sub
 _Eq = compile(r'^([^=]*)==(.*)').sub
-pythonified = lambda expr: _Eq( r'\1-(\2)', _Mul( r'\1*', expr ))
+pythonify = lambda expr: _Eq( r'\1-(\2)', _Mul( r'\1*', expr ))
 dictmap = lambda l, f: dict(zip( l, map( f, l )))
 params = lambda dict: ', '.join(['%s=%r' %p for p in dict.iteritems()])
 
-
-_skip = dictmap("COSINEQ", Symbol) # Skip one-letter definitions in sympy
-
-def _systemize(s):
-    if isinstance( s, Basic ): yield s
-    elif isinstance( s, str ):
-        try:
-            yield sympify( pythonified(s), _skip.copy() )
-        except:
-            raise TypeError('Cannot parse %s.'% s)
-    elif isinstance( s, (Iterable, Matrix) ):
-        for ss in s:
-            for e in _systemize(ss): yield e
-    else: raise TypeError('No system: %r'%s)
-
-def systemize(s):
-    "Compile a system of equations"
-    return tuple(_systemize(s))
-
-
-def _system_parameters(s):
-    if isinstance( s, (Iterable, Matrix) ):
-        for ss in s:
-            for item in _system_parameters(ss): yield item
-    else:
-        for a in s.atoms(Symbol): yield (a.name, a)
-
-def system_parameters(s):
-    "Extract parameters from a system of equations"
-    return dict(_system_parameters(s))
-
 class System( Basic ):
     """
-    A utility for defining and using mathematical expressions.
-    
-    String expressions are pythonified before evaluation. This inserts * in
+    Associates attributes with any object.
+
+    Attributes can be supplied as dictionaries, modules, objects or keyword
+    arguments during the association or when called.
+
+    If the object is a string, it is assumed to contain a mathematical
+    expression. It is then pythonified before evaluation: inserts * in
     implicit multiplications, replaces ^ with ** and replaces ==<rhs> with
     -(<rhs>).
 
-    Symbols are solved for when used as an attribute.
+    An expression is evaluated as far as possible using the supplied attributes
+    as substitutions. Remaining symbols can be solved for by using the symbol as
+    an attribute. 
 
-    Expressions are simplified when dictionaries, modules, objects or keyword
-    arguments are supplied, during creation or when called. Since expressions
-    are managed in sympy which has poor interoperability with quantities and
-    arrays, any such data is temporarily converted to symbols until all
-    parameters are defined.
+    Note: Expression are managed in sympy which has poor interoperability with
+    quantities and arrays. Any such data is terefore temporarily converted to
+    implicit symbols until all parameters of the expression is defined. These
+    are stored in a special attribute 'implicit'.
     
     Example 1: Solving for parameters in an expression.
     >>> charging = System('E == (n - C V/e)^2 e^2/(2 C)'); charging.E
-    -V*e*n + 0.5*C*V**2 + 0.5*e**2*n**2/C
+    (-2*C*V*e*n + C**2*V**2 + e**2*n**2)/(2*C)
     >>> E = charging.E(V=0); E
     0.5*e**2*n**2/C
-
-    Example 2: Using physical constants, arrays and uncertainties.
-    >>> from quantities import constants, K, F, eV
-    >>> from statistics import std, confidence
-    >>> from numpy import array
-    >>> blockade = System( (E(n=1) - E(n=0)) - Symbol('dE'), constants )
-    >>> C = blockade.C; C # showing temporary symbols
-    1.28348474774783e-38*_A**2*_s**2/dE /. _s=UnitTime('second', 's'), _A=UnitCurrent('ampere', 'A')
-    >>> kT = constants.k*array([ 300, 600, 900 ])*K
-    >>> C = blockade.C(dE = kT).rescale(F); C
-    array([  3.09874425e-18,   1.54937213e-18,   1.03291475e-18]) * F
-    >>> C0 = confidence( std( 1, .1 )*1e-18*F, 0.99 ); C0
-    array([  6.71004729e-19,   1.32899527e-18]) * F
-    >>> blockade( C = C0.max(), dE = kT ).rescale(eV)
-    array([ 0.03442571,  0.00857368, -0.01727835]) * eV
+    >>> from quantities import constants, F, eV
+    >>> En = E( constants, aF=1E-18*F, C = System('n*0.1 aF')); En.rescale(eV)
+    0.801088222000001*eV*n
+    >>> En(n = 1).rescale(eV)
+    array(0.8010882220000002) * eV
     """
     
-    # object data - names are defined here to make __getattribute__ easier.
-    source_system       = None # source equations
-    source_parameters   = None # source parameter values
-    system              = None # compiled equations
-    defined             = None # used parameters values
-    implicit            = None # stuff that can mess upp sympy
-    default             = None # remaning symbols and messy stuff
-    subsystem           = None # simplified equations
+    # Object data - defined here to make __getattribute__ easier.
+    model       = None # Input sympy equation or numpy array.
+    environment = None # Dictionary of parameter values.
+    applied     = None # Substitutions made.
+    result      = None # Resulting sympy equation or numpy array.
+    implicit    = None # Hidden arrays and units in resulting equation.
+    remaining   = None # Remaining symbols in resulting equation.
     
-    def __new__( cls, * scopes, ** scope ):
-        # Collect equations and parameters
-        source_system = list() # Matrix, Basic, str, list
-        source_parameters = dict() # anything else
-        for s in scopes + (scope,):
-            if isinstance( s, (Matrix, Basic, str, list) ):
-                source_system.append(s)
-            elif isinstance( s, dict ): source_parameters.update(s)
-            else: source_parameters.update(s.__dict__)
-        self = Basic.__new__( cls, source_system, source_parameters )
-        # Build symbolic expressions and parameters
-        system = systemize(source_system)
-        parameters = system_parameters(system)
-        defined   = dictmap( set(parameters).intersection(source_parameters),
-                             source_parameters.get )
-        # Work around objects that sympify can't handle. Can be fixed by
-        # improving the interoperability between sympy, numpy and quantity. 
-        quantities = dict([(n, v) for n, v in defined.iteritems()
-                           if isinstance( v, Quantity ) and not n[0]=='_'])
-        implicit = dict([
-            (n, v) for n, v in defined.iteritems()
-            if isinstance( v, Iterable ) and not n in quantities])
-        welldefined = dictmap(
-            set(defined).difference(quantities).difference(implicit),
-            defined.get )
-        undefined = dictmap( set(parameters).difference(defined),
-                             parameters.get )
-        if quantities:
-            # Collect all base units
-            qitem = lambda q: ('_'+ q.symbol, q)
-            base_units = dict([qitem(d._default_unit) for d in dimensions])
-            # Expand quantities to base units and replace those with symbols
-            units = set()
-            for n, q in quantities.iteritems():
-                q = q.simplified
-                k = 1
-                for u, d in q.dimensionality.iteritems():
-                    unit = '_'+ u.symbol
-                    k *= Symbol(unit)**d
-                    units.add(unit)
-                if q.shape or isinstance(q, UncertainQuantity):
-                    n_ = n +'_'
-                    implicit[n_] = q/q.units
-                    welldefined[n] = Symbol(n_) * k
-                else:
-                    welldefined[n] = q.magnitude * k
-            implicit.update(dictmap( units, base_units.get ))
-        # Simplify system into subsystem using parameters 
-        try:
-            subsystem = Matrix(system).subs(welldefined).evalf()
-        except TypeError, err:
-            raise TypeError('%s /. %s: %s' % (system, params(welldefined), err))
-        self.source_system = source_system
-        self.source_parameters = source_parameters
-        self.system = system
-        self.defined = defined
-        self.implicit = implicit
-        if undefined:
-            if subsystem.shape == (1,1): subsystem = subsystem[0,0]
-            self.default = undefined
-            self.default.update(implicit)
-            self.subsystem = subsystem
-        else:
-            if subsystem.shape == (1,1): subsystem = subsystem[0,0]
-            else: subsystem = list(subsystem)
-            self.default = dict()
+    _skip = dictmap("COSINEQ", Symbol) # Override these in sympy
+
+    def __new__( cls, model, * setup, ** operation ):
+        parameters = setup + (operation,)
+        self = Basic.__new__( cls, model, * parameters )
+        # Collect parameter values
+        environment = dict()
+        for s in parameters:
+            if isinstance( s, dict ): environment.update(s)
+            elif isinstance( s, System ): environment.update(s.environment)
+            else: environment.update(s.__dict__)
+        E = self.environment = environment
+        M = self.model = (sympify( pythonify(model), self._skip.copy() )
+                          if isinstance( model, str ) else
+                          (model[0] if len(model) == 1 else Matrix(model))
+                          if isinstance( model, list ) else model)
+        S = symbols = (dict([(a.name, a) for a in M.atoms(Symbol)])
+                       if isinstance( M, Basic ) else dict())
+        # Find applicable substitutions and remaning undefined parameters.
+        A = self.applied = dictmap( set(S).intersection(E), E.get )
+        I = self.implicit = dict()
+        R = self.remaining = dictmap( set(S).difference(A), S.get )
+        if A:
+            # Evaluate all sub-expressions.
+            for n, v in A.copy().iteritems():
+                if isinstance( v, Basic ) and not v == Symbol(n) \
+                   and v.atoms(Symbol):
+                    v = v(E) if isinstance( v, System ) else System( v, E )
+                    A.update(v.applied)
+                    A[n] = v.result
+                    I.update(v.implicit)
+                    R.update(v.remaining)
+            # Workaround objects that sympy can't handle. Can be fixed
+            # by improving the interoperability between sympy and numpy.
+            Q = dict([(n, v) for n, v in A.iteritems()
+                      if isinstance( v, Quantity ) and not n[0]=='_'])
+            I.update([(n, v) for n, v in A.iteritems()
+                      if isinstance( v, Iterable ) and not n in Q])
+            OK = dictmap( set(A).difference(Q).difference(I), A.get )
+            # A = OK + I + Q now.
+            if Q:
+                # Collect all base units
+                base_units = dict([('_'+ u.symbol, u)
+                                   for u in [d._default_unit
+                                             for d in dimensions]])
+                # Expand quantities to base units and replace those with symbols
+                units = set()
+                for n, q in Q.iteritems():
+                    q = q.simplified
+                    k = 1
+                    for u, d in q.dimensionality.iteritems():
+                        unit = '_'+ u.symbol
+                        k *= Symbol(unit)**d
+                        units.add(unit)
+                    if q.shape or isinstance(q, UncertainQuantity):
+                        n_ = n +'_'
+                        I[n_] = q/q.units
+                        OK[n] = Symbol(n_) * k
+                    else:
+                        OK[n] = q.magnitude * k
+                I.update(dictmap( units, base_units.get ))
+            # End of workaround. A = OK + I now.
             try:
-                y = lambdify( implicit, subsystem )(* implicit.values())
-            except TypeError, err:
-                raise TypeError('%s /. %s: %s' % (subsystem, params(implicit), err))
-            self.subsystem = y
+                result = M.subs(OK).evalf()
+            except Exception, err:
+                raise Exception('%s /. %s: %s'% (M, params(OK), err))
+            if not R and isinstance( result, Basic ):
+                # Only implicit parameters left - ok to evaluate numerically. 
+                try:
+                    result = lambdify( I, result )(* I.values())
+                except Exception, err:
+                    raise Exception('%s /. %s: %s' % (result, params(I), err))
+                self.implicit = dict()
+            self.result = result
+        else:
+            self.result = M
+            self.implicit = dict()
         return self
 
-    def __call__( self, * scopes, ** scope ):
-        return System( self.subsystem, self.source_parameters, self.implicit,
-                       * scopes, ** scope )
+    def __call__( self, * setup, ** operation ):
+        return System( self.model, self.environment, self.implicit,
+                       * setup, ** operation )
+        
+    def __getitem__( self, k ):
+        return self.result.__getitem__(k)
+
+    def rescale( self, unit ):
+        if isinstance( self.result, Quantity ):
+            return self.result.rescale(unit)
+        else:
+            symunits = System('x*u', u=unit)/Symbol('x')
+            return self.result/symunits * Symbol(unit.symbol)       
         
     def __getattribute__( self, name, default=None ):
         if name in System.__dict__:
             return object.__getattribute__( self, name )
-        if name in self.source_parameters: return self.source_parameters[name]
-        if name in self.default:
-            if isinstance(self.default[name], Symbol):
-                "Use case: Solving a parameter in the expression"
-                return System( solve( self.subsystem, Symbol(name) ),
-                               self.source_parameters, self.implicit )
-            else: return self.default[name]
-        subsystem = self.subsystem
+        if name in self.environment: return self.environment[name]
+        if name in self.implicit: return self.implicit[name]
+        if name in self.remaining: 
+            return System( solve( self.result, Symbol(name) ), self.environment,
+                           self.implicit )
+        result = self.result
         if default==None:
             try:
-                return getattr( subsystem, name )
+                return getattr( result, name )
             except AttributeError:
                 raise AttributeError("%r has no attribute '%s'."%( self, name ))
-        return getattr( subsystem, name, default )
-    
-    def __str__(self):
-        return str(self.source_system)
+        return getattr( result, name, default )
     
     def __repr__(self):
-        if self.implicit and self.default:
-            return '%r /. %s' % (self.subsystem, params(self.implicit))
-        return repr(self.subsystem)
+        if self.implicit:
+            return '%r /. %s' % (self.result, params(self.implicit))
+        return repr(self.result)
 
+    __str__ = __repr__
 
 if __name__ == '__main__':
     import doctest
