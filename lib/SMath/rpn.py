67ae34b6ca12e7bb5831b003d2dd283b6473e232
Joakim Pettersson <joakim.pettersson@join.se>
2019-04-18 11:25:13 +0200

    REFACTOR(SMath): Prepare for importing file
    * Moved rpn calculator into new submodule 'rpn'.
    * Added small SMath file to extract rpn from.

---
+++ lib/SMath/rpn.py
@@ -0,0 +1,133 @@
+"""
+RPN calculator
+
+Operators are (currently) defined like this:
+operators['+'] = operator(2, lambda x, y: x + y)
+operators['not'] = operator(1, lambda x: not x)
+"""
+
+
+from sympy import sympify, Basic, Function
+from sympy.utilities.lambdify import implemented_function
+
+
+stack = []
+declarations = []
+operators = {}
+substitutions = {}
+replacements = {}
+
+
+def use(e):
+    """sympify, substitute and replace e"""
+    e = sympify(e).subs(substitutions)
+    for r in replacements.items():
+        e = e.replace(*r)
+    return e
+
+
+def assignment():
+    """value name --- ; substitutions[name] = use(value)"""
+    name = stack.pop()
+    substitutions[name] = use(stack.pop())
+
+
+def operator(n, op):
+    """pars[-n:] --- op(*pars)"""
+    return lambda: stack.append(op(*reversed([use(stack.pop()) for i in range(n)])))
+
+
+def declare():
+    """pars[-n:] name n --- ; Begin capturing code for a function <name>(*pars)"""
+    global stack
+    n, name = int(stack.pop()), stack.pop()
+    names, stack = stack[-n:], stack[:-n]
+    declarations.append((name, names, []))
+
+
+def define():
+    """ --- ; End capturing code for a function"""
+    name, names, code = declarations.pop()
+    expression = execute(code).pop()
+    f = eval("lambda %s: %r" % (", ".join(names), expression))
+    replacements[Function(name)] = implemented_function(name, f)
+
+
+def call():
+    """pars[-n:] name n --- <name>(*pars)"""
+    n, name = int(stack.pop()), stack.pop()
+    f = replacements.get(name, None) or Function(name)
+    return stack.append(f(*reversed([use(stack.pop()) for i in range(n)])))
+
+
+def parse(line):
+    commands = []
+    for t in line.split():
+        # next line, using default parameters, is intentional.
+        commands.append(operators.get(t, lambda t=t: stack.append(t)))
+    return commands
+
+
+def execute(code):
+    for t in code:
+        if declarations and t != define:
+            declarations[-1][-1].append(t)
+        elif isinstance(t, Basic):
+            stack.append(t)
+        else:
+            t()
+    return stack
+
+
+operators.update({
+    '+': operator(2, lambda x, y: x + y),
+    '-': operator(2, lambda x, y: x - y),
+    '*': operator(2, lambda x, y: x * y),
+    '/': operator(2, lambda x, y: x / y),
+    '=': assignment,
+    ':': declare,
+    ';': define,
+    '()': call})
+
+
+def rpn(line):
+    """
+    Parse and execute math in reverse polish notation
+
+    rpn(line) maintains and returns a global stack of operands.
+    The line can contain operands (pushed to the stack) and operators on the stack.
+
+    rpn   --- result
+    ================
+    a b + --- a+b
+    a b - --- a-b
+    a b * --- a*b
+    a b / --- a/b
+    a b = b --- a
+    x y f 2 : x y * ; a b f 2 () --- a*b
+
+    Notes:
+    * Operands are sympified and evaluated within the current context
+    (variable substitutions and function replacements) before being used.
+    * The complete stack is numerically evaluated before being returned.
+
+    >>> rpn('2 pi * 1 2 / -')
+    [5.78318530717959]
+    >>> rpn('b a + fun 2 () 2 3 fun 2 ()')
+    [fun(5.78318530717959, a + b), fun(2, 3)]
+    >>> rpn('0 a =')
+    [fun(5.78318530717959, b), fun(2, 3)]
+    >>> rpn('x y fun 2 : x y * ;')
+    [fun(5.78318530717959, b), 6.00000000000000]
+    >>> rpn('1 b =')
+    [5.78318530717959, 6.00000000000000]
+    """
+    execute(parse(line))
+    for i, t in enumerate(stack):
+        stack[i] = use(t).evalf()
+    return stack
+
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
